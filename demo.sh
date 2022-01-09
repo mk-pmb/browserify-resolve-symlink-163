40119b076e17f433c659bd228fb3ab4f210cd2bc
mk-pmb <mk-pmb@users.noreply.github.com>
2022-01-09 05:43:15 +0100

    Set env export CI=true in bash script

--- demo.sh
+++ demo.sh
@@ -6,6 +6,7 @@ function demo_main () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   local SELFPATH="$(readlink -m -- "$BASH_SOURCE"/..)"
 
+  export CI=true
   export HOME="$SELFPATH/prepare"
   cd -- "$HOME" || return $?
   vdo npm --versions
