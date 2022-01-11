61ca02eafc3198afa3b0ee9cda2a8ade99e1e895
mk-pmb <mk-pmb@users.noreply.github.com>
2022-01-11 21:34:44 +0100

    Print version of "resolve" module

---
+++ app.js
@@ -0,0 +1,1 @@
+home/demo/my-cool-app/app.js
--- home/demo/my-cool-app/app.js
+++ home/demo/my-cool-app/app.js
@@ -2,9 +2,16 @@
 
 require('p-fatal');
 const resolve = require('resolve');
+const resolvePkgInfo = require('resolve/package.json');
 const promisify = require('pify');
 
 require('locally-symlinked-module-163'); // Help my linter detect this dep
+
+const resoOpt = {
+};
+
+console.log('"resolve" module version:', resolvePkgInfo.version,
+  '| options:', resoOpt);
 
 function shorten(s) {
   return s.replace(/^\/\S+(?=\/[a-z\-]+ink-163\/)/g, '/…');
@@ -13,15 +20,14 @@ function shorten(s) {
 }
 
 async function discover(id) {
+  console.log('');
   console.log(id + '?');
   console.log('    node.js resolver:',
     shorten(require.resolve(id)));
 
-  function wrap(cb) { resolve(id, cb); }
+  function wrap(cb) { resolve(id, resoOpt, cb); }
   console.log('    "resolve" module:',
     shorten(await promisify(wrap)().then(null, String)));
-
-  console.log('');
 }
 
 async function main() {
