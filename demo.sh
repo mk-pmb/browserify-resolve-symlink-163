28d2c21ae8fb6423ddd530c9affce31babf4e8c3
mk-pmb <mk-pmb@users.noreply.github.com>
2022-01-17 05:59:04 +0100

    Test with node v16, too.

--- demo.sh
+++ demo.sh
@@ -15,6 +15,7 @@ function demo_main () {
   export HOME="$SELFPATH/prepare"
   cd -- "$HOME" || return $?
   vdo npm install . || return $?
+  maybe_fix_node_modules_symlink_broken_by_npm || return $?
 
   export HOME="$SELFPATH"/home/demo
   cd -- "$HOME/my-cool-app" || return $?
@@ -26,6 +27,7 @@ function vdo () {
   local SHORT_PWD="$PWD"
   SHORT_PWD="${SHORT_PWD/#${SELFPATH%/*}\//\/…\/}"
   local DESCR="$*"
+  DESCR="${DESCR#eval }"
   [ "${#DESCR}" -lt 50 ] || DESCR="${DESCR:0:49}…"
   echo "----- 8< --== $DESCR @ $SHORT_PWD ==-- 8< ----- 8< ----- 8< ----- 8< -----"
   "$@"
@@ -33,6 +35,24 @@ function vdo () {
   echo "----- >8 --== $DESCR => rv=$RV  ==-- >8 ----- >8 ----- >8 ----- >8 -----"
   echo
   return "$RV"
+}
+
+
+function maybe_fix_node_modules_symlink_broken_by_npm () {
+  # npm v8 insists on replacing our perfectly fine `node_modules` symlink
+  # with a regular subdirectory. (ref: #node.js IRC logs 2022-01-17T01:52:40Z)
+  [ -L node_modules ] && return 0
+  mv --no-target-directory -- node_modules nm || return $?
+  git checkout -- node_modules || return $?
+  rm --recursive -- \
+    nm/.bin \
+    nm/.package-lock.json \
+    2>/dev/null || true
+  mv --target-directory=node_modules/ -- nm/[a-z]*/ || return $?
+  rmdir -- nm && return 0
+  echo "W: rmdir failed, rv=$?" >&2
+  vdo ls -lFa -- nm >&2
+  return 8
 }
 
 
